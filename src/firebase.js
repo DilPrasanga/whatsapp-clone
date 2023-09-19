1679d97dbaf091cf3210ba2c2697ec23b7ef6f68
DilPrasanga <dilanik@opusxenta.com>
2023-09-19 13:42:06 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcWvLQie_NIxPD-EQMNpp0Uxh1Cr5fUrA",
+    authDomain: "whatsapp-clone-33ebc.firebaseapp.com",
+    projectId: "whatsapp-clone-33ebc",
+    storageBucket: "whatsapp-clone-33ebc.appspot.com",
+    messagingSenderId: "1004025287127",
+    appId: "1:1004025287127:web:70431a03ffe36fe6727b92"
+  };
